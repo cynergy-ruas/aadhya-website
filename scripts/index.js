7e4a5b604ca8480d5247547a2d65c6df2bf49f8e
shikhar <shikharswat64@gmail.com>
2020-02-07 08:02:05 +0530

    bug fixes, organized file structure

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ $(window).on("load", function() {
 function showPage() {
   // $("loadingDiv").fadeOut();
   // $("myDiv").fadeIn();
-  getElementById("myDiv").style.display = "block";
+  document.getElementById("myDiv").style.display = "block";
   $("#loadingDiv").remove();
 }
 var animation = bodymovin.loadAnimation({
