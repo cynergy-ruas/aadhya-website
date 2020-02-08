0f8ffaf25bbcd0aef664b7d5462eff15f4858802
subhendu17620 <subhendu17620@gmail.com>
2020-02-08 22:18:13 +0530

    completed findus page

---
+++ scripts/map.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var ruas = { lat: 13.0183642, lng: 77.5078933 };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 20,
+    center: ruas
+  });
+  var marker = new google.maps.Marker({
+    position: ruas,
+    map: map
+  });
+}
